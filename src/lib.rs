4f1b011859dca94e10a13fc1022c9afca93e9e13
Paris DOUADY <paris.douady@hotmail.fr>
2024-10-11 17:04:26 +0200

    impl basic STA and critical path analysis

---
+++ src/lib.rs
@@ -0,0 +1,302 @@
+use std::fmt::{Display, Formatter};
+use ordered_float::OrderedFloat;
+use rustc_hash::{FxHashMap, FxHashSet};
+use sdfparse::{SDFBus, SDFDelay, SDFIOPathCond, SDFPath, SDFPort, SDFPortEdge, SDFValue};
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct SDFEdge {
+    pub dst: SDFNode,
+    pub delay_pos: f32,
+    pub delay_neg: f32,
+}
+
+pub type SDFNode = String;
+
+pub struct SDFGraph {
+    pub graph: FxHashMap<SDFNode, Vec<SDFEdge>>,
+    pub reverse_graph: FxHashMap<SDFNode, Vec<SDFEdge>>,
+    pub inputs: Vec<SDFNode>,
+    pub outputs: Vec<SDFNode>,
+}
+
+fn unique_name(path: &SDFPath) -> SDFNode {
+    let mut name = String::new();
+    for part in &path.path {
+        name.push_str(&part);
+        name.push('/');
+    }
+    name.pop();
+    match path.bus {
+        SDFBus::None => {}
+        SDFBus::SingleBit(b) => {
+            name.push('[');
+            name.push_str(&b.to_string());
+            name.push(']');
+        }
+        SDFBus::BitRange(_, _) => {
+            unimplemented!("SDFBus::BitRange");
+        }
+    }
+    name
+}
+
+fn unique_name_port(cell_name: &SDFNode, port: &SDFPort) -> SDFNode {
+    let mut name = cell_name.clone();
+    name.push('/');
+    name.push_str(&port.port_name);
+    match port.bus {
+        SDFBus::None => {}
+        SDFBus::SingleBit(b) => {
+            name.push('[');
+            name.push_str(&b.to_string());
+            name.push(']');
+        }
+        SDFBus::BitRange(_, _) => {
+            unimplemented!("SDFBus::BitRange");
+        }
+    }
+    name
+}
+
+fn extract_delay(value: &SDFValue) -> f32 {
+    match *value {
+        SDFValue::None => 0.0,
+        SDFValue::Single(v) => v,
+        SDFValue::Multi(v, _, _) => {
+            v.unwrap_or(0.0)
+        }
+    }
+}
+
+
+impl SDFGraph {
+    pub fn new(sdf: &sdfparse::SDF, check_cycle: bool) -> Self {
+        let mut graph: FxHashMap<_, _> = FxHashMap::with_capacity_and_hasher(sdf.cells.len(), Default::default());
+        let mut reverse_graph: FxHashMap<_, _> = FxHashMap::with_capacity_and_hasher(sdf.cells.len(), Default::default());
+
+        for cell in &sdf.cells {
+            let cell_name = unique_name(cell.instance.as_ref().unwrap_or(&SDFPath {
+                path: vec![],
+                bus: SDFBus::None,
+            }));
+
+            for delay in &cell.delays {
+                match delay {
+                    SDFDelay::Interconnect(inter) => {
+                        let [up, down] = &*inter.delay else {
+                            panic!("Interconnect delay is not of length 2 (up, down) for {:?}", cell.instance);
+                        };
+
+                        let a_name = unique_name(&inter.a);
+                        let b_name = unique_name(&inter.b);
+
+                        graph.entry(a_name.clone()).or_insert_with(Vec::new).push(SDFEdge {
+                            dst: b_name.clone(),
+                            delay_pos: extract_delay(up),
+                            delay_neg: extract_delay(down),
+                        });
+                        graph.entry(b_name.clone()).or_insert_with(Vec::new);
+
+                        reverse_graph.entry(b_name).or_insert_with(Vec::new).push(SDFEdge {
+                            dst: a_name.clone(),
+                            delay_pos: extract_delay(up),
+                            delay_neg: extract_delay(down),
+                        });
+                        reverse_graph.entry(a_name).or_insert_with(Vec::new);
+                    }
+                    SDFDelay::IOPath(cond, io) => {
+                        let SDFIOPathCond::None = cond else {
+                            panic!("IOPathCond is not None for {:?}", cell.instance);
+                        };
+
+                        if !matches!(io.a.edge_type, SDFPortEdge::None) {
+                            panic!("edge_type is not None for {:?}", cell.instance);
+                        }
+
+                        let a_name = unique_name_port(&cell_name, &io.a.port);
+                        let b_name = unique_name_port(&cell_name, &io.b);
+
+                        let [up, down] = &*io.delay else {
+                            panic!("Interconnect delay is not of length 2 (up, down) for {:?}", cell.instance);
+                        };
+
+                        graph.entry(a_name.clone()).or_insert_with(Vec::new).push(SDFEdge {
+                            dst: b_name.clone(),
+                            delay_pos: extract_delay(up),
+                            delay_neg: extract_delay(down),
+                        });
+                        graph.entry(b_name.clone()).or_insert_with(Vec::new);
+
+                        reverse_graph.entry(b_name).or_insert_with(Vec::new).push(SDFEdge {
+                            dst: a_name.clone(),
+                            delay_pos: extract_delay(up),
+                            delay_neg: extract_delay(down),
+                        });
+                        reverse_graph.entry(a_name).or_insert_with(Vec::new);
+                    }
+                }
+            }
+        }
+
+        let mut outputs: Vec<SDFNode> = Vec::new();
+        let mut inputs: Vec<SDFNode> = Vec::new();
+
+        for (key, edges) in &graph {
+            if edges.is_empty() {
+                outputs.push(key.clone());
+            }
+        }
+
+        for (key, edges) in &reverse_graph {
+            if edges.is_empty() {
+                inputs.push(key.clone());
+            }
+        }
+
+        numeric_sort::sort_unstable(&mut inputs);
+        numeric_sort::sort_unstable(&mut outputs);
+
+        if check_cycle {
+            if Self::has_cycle(&graph, &inputs) {
+                panic!("graph has cycle :(");
+            }
+        }
+
+        SDFGraph {
+            graph,
+            reverse_graph,
+            inputs,
+            outputs,
+        }
+    }
+
+    fn has_cycle_dfs(graph: &FxHashMap<SDFNode, Vec<SDFEdge>>, node: &SDFNode, visited: &mut FxHashSet<SDFNode>, stack: &mut FxHashSet<SDFNode>) -> bool {
+        if stack.contains(node) {
+            return true;
+        }
+
+        if visited.contains(node) {
+            return false;
+        }
+
+        visited.insert(node.clone());
+        stack.insert(node.clone());
+
+        for edge in &graph[node] {
+            if Self::has_cycle_dfs(graph, &edge.dst, visited, stack) {
+                return true;
+            }
+        }
+
+        stack.remove(node);
+        false
+    }
+
+    pub fn has_cycle(graph: &FxHashMap<SDFNode, Vec<SDFEdge>>, inputs: &[SDFNode]) -> bool {
+        let mut visited = FxHashSet::default();
+        let mut stack = FxHashSet::default();
+
+        for node in inputs {
+            if Self::has_cycle_dfs(graph, node, &mut visited, &mut stack) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+pub struct SDFGraphAnalyzed {
+    pub max_delay: FxHashMap<SDFNode, f32>,
+}
+
+impl SDFGraph {
+    /// Propagate delays through the graph and return the maximum delay for each node.
+    /// The maximum delay is the maximum time it takes for a signal to propagate from the inputs to the node.
+    pub fn analyze(&self) -> SDFGraphAnalyzed {
+        let mut max_delay = FxHashMap::default();
+
+        let mut queue = priority_queue::PriorityQueue::with_capacity(self.graph.len());
+        let mut visited: FxHashSet<_> = Default::default();
+
+        for node in &self.inputs {
+            queue.push(node.clone(), OrderedFloat(0.0));
+        }
+
+        while let Some((node, OrderedFloat(delay))) = queue.pop() {
+            max_delay.insert(node.clone(), delay);
+            visited.insert(node.clone());
+
+            let edges = &self.graph[&node];
+            for edge in edges {
+                let delay = delay + f32::max(edge.delay_pos, edge.delay_neg);
+
+                if !visited.contains(&edge.dst) {
+                    queue.push(edge.dst.clone(), OrderedFloat(delay));
+                }
+            }
+        }
+
+        SDFGraphAnalyzed {
+            max_delay,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Transition {
+    /// Positive transition. 0 -> 1
+    Pos,
+    /// Negative transition. 1 -> 0
+    Neg,
+}
+
+impl Display for Transition {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Transition::Pos => write!(f, "↗"),
+            Transition::Neg => write!(f, "↘"),
+        }
+    }
+}
+
+impl SDFGraphAnalyzed {
+    /// Extract the path of transitions that led to the max delay of the given output node.\
+    /// The path is a list of (node, transition, delay) tuples, going from the start to the output.\
+    ///
+    /// **Example**: `[(1, Pos, 0.1), (2, Neg, 0.2)]` means that the transition from 1 to 2 was a positive transition with a delay of 0.1, and the transition from 2 to the output was a negative transition with a delay of 0.2.
+    ///
+    /// **Note**: The output is _not_ included in the path (since it doesn't do any transitions itself).
+    pub fn extract_path(&self, graph: &SDFGraph, output: &SDFNode) -> Vec<(SDFNode, Transition, f32)> {
+        let mut path = Vec::new();
+
+        fn find_prev(graph: &SDFGraph, node: &SDFNode, max_delay: &FxHashMap<SDFNode, f32>) -> Option<(SDFNode, Transition, f32)> {
+            let edges = &graph.reverse_graph[node];
+            let delay = max_delay[node];
+            let mut prev = None;
+            for edge in edges {
+                let prev_delay = max_delay[&edge.dst];
+
+                //println!("{} -> {}\t{}, ↗{:.3} ↘{:.3} = {}", edge.dst, node, prev_delay, edge.delay_pos, edge.delay_neg, delay);
+
+                if prev_delay + edge.delay_pos == delay {
+                    prev = Some((edge.dst.clone(), Transition::Pos, prev_delay));
+                } else if prev_delay + edge.delay_neg == delay {
+                    prev = Some((edge.dst.clone(), Transition::Neg, prev_delay));
+                }
+            }
+            prev
+        }
+
+        let mut node = output.clone();
+
+        while let Some((prev_node, transition, delay)) = find_prev(graph, &node, &self.max_delay) {
+            path.push((prev_node.clone(), transition, delay));
+            node = prev_node;
+        }
+
+        path.reverse();
+
+        path
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::cmp::Reverse;
+use std::fs::read_to_string;
+use ordered_float::OrderedFloat;
+use stars::{SDFGraph, SDFNode};
+
 fn main() {
-    println!("Hello, world!");
+    let path_to_parse = std::env::args_os().nth(1).expect("No argument given");
+
+    let content = read_to_string(path_to_parse).expect("Could not read SDF file");
+
+    let sdf = sdfparse::SDF::parse_str(&content).expect("Could not parse SDF");
+
+    let graph = SDFGraph::new(&sdf, true);
+
+    // print_graph(&graph, &mut keys);
+
+    let analysis = graph.analyze();
+
+    let mut outputs_with_delay = graph.outputs.iter().map(|output| {
+        (output, analysis.max_delay.get(output).unwrap())
+    }).collect::<Vec<_>>();
+
+    outputs_with_delay.sort_by_key(|(_, delay)| Reverse(OrderedFloat(**delay)));
+
+    for (output, delay) in outputs_with_delay.into_iter().take(1) {
+        println!("{}:\t{:.3}", output, delay);
+        let path = analysis.extract_path(&graph, output);
+        let path_l = path.len();
+        for (i, (node, transition, delay)) in path.into_iter().enumerate() {
+            print!("{} {}{:.3}", node, transition, delay);
+            if i + 1 < path_l {
+                print!(" -> ");
+            }
+        }
+        println!();
+    }
+}
+
+#[allow(dead_code)]
+fn print_graph(graph: &SDFGraph) {
+    let mut keys: Vec<&SDFNode> = graph.graph.keys().collect();
+
+    numeric_sort::sort_unstable(&mut keys);
+
+    for inputs in &graph.inputs {
+        println!("input: {}", inputs);
+    }
+
+    for outputs in &graph.outputs {
+        println!("output: {}", outputs);
+    }
+
+    for key in keys {
+        let edges = graph.graph.get(key).unwrap();
+        for edge in edges {
+            println!("{} -> {}\t↗{:.3} ↘{:.3}", key, edge.dst, edge.delay_pos, edge.delay_neg);
+        }
+    }
 }
